61b92b9055d24b2a0840eaa3e3309604b3def727
Lydia B <lydiabaik@gmail.com>
2019-12-20 07:20:02 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -11,17 +11,19 @@ def third_challenge
         },
       :hero => {
         name: "Romeo",
-        age: "15"
+        age: "15",
         status: "alive"
         },
       :hero_friends => []
    },
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        name: "Lord Capulet",
+        age: "50"
         },
       :matriarch => {
-        #your key/value pairs here
+        name: "Lady Capulet",
+        age: "51"
         },
       :heroine => {
         #your key/value pairs here
